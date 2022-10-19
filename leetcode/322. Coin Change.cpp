8f291b46a7b9a58d09228c8a454b1fc5909ee98e
ihritwik <hritwiks.tgcs@gmail.com>
2022-10-19 18:00:51 -0400

    Dynamic Programming - bottoms down approach

---
+++ leetcode/322. Coin Change.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        // Dynamic Programming (Bottom up approach)
+        
+        // Initialize DP table and fill all elements in it with max value (amount +1)
+        int dp[amount+1];
+        std::fill_n (dp, amount+1, amount+1);
+        dp[0] = 0;
+        
+        for (int coin=1; coin<=coins.size(); coin++)
+        {
+            for (int i = coins[coin-1]; i<=amount; i++)
+            {
+                dp[i] = std::min(dp[i], dp[i-coins[coin-1]] + 1);
+            }   
+        }
+
+        if (dp[amount] >= amount+1)
+        {
+            return -1;
+        }    
+        else{
+            return (dp[amount]);
+        }
+    }
+};
