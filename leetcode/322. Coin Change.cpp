66f71c928fac35f9690edffd57f2e5a4b4c77ae3
ihritwik <hritwiks.tgcs@gmail.com>
2022-10-20 00:26:03 -0400

    Dynamic Programming and Greedy Algorithm

--- leetcode/322. Coin Change.cpp
+++ leetcode/322. Coin Change.cpp
@@ -7,10 +7,9 @@ using namespace std;
 
 class Solution {
 public:
+//  Approach 2: Dynamic Programming (Bottom up approach). Time complexity : O(S*n), Space Complexity O(S)
     int coinChange(vector<int>& coins, int amount) 
-    {
-        // Dynamic Programming (Bottom up approach). Time complexity : O(S*n), Space Complexity O(S)
-        
+    {   
         // Initialize DP table and fill all elements in it with max value (amount +1)
         int dp[amount+1];
         std::fill_n (dp, amount+1, amount+1);
@@ -33,3 +32,34 @@ public:
         }
     }
 };
+
+// Approach 1: Recursive Greedy approach
+
+    // int helper_recursion(vector<int>& coins, int amount, int sz_real)
+	// {
+	// 	//sort(coins.begin(), coins.end());
+    //     //base case
+    //     if (coins.size()==0){
+    //         return 0;
+    //     }
+        
+    //     else if (coins.size()==1) {
+    //         if (amount == coins[0]){
+    //             return 1;
+    //         }
+    //         else
+    //             return -1; 
+    //     }
+    //     else if (amount>=coins[0]){
+    //         int freq = amount/coins[sz_real-1]; 
+    //         //min_coins += freq;
+    //         amount = amount%coins[sz_real-1]; 
+    //         return (freq + helper_recursion(coins, amount, sz_real-1));   
+    //     }
+    //     return 0;
+	// }
+
+    // int coinChange(vector<int>& coins, int amount) 
+    // {
+    //     return (helper_recursion(coins, amount, coins.size()));
+    // }
