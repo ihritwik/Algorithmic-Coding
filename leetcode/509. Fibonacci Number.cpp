b0612b42f15a612be37451d30f78c809fbb4975e
ihritwik <hritwiks.tgcs@gmail.com>
2023-01-05 17:11:39 -0500

    fibonacci_recursion-bottom up dynamic programming

--- leetcode/509. Fibonacci Number.cpp
+++ leetcode/509. Fibonacci Number.cpp
@@ -6,9 +6,23 @@
 using namespace std;
 class Solution {
 public:
+
+    // Recursion Approach : Time complexity (2^n)
+    
+    // int fib(int n) {
+    //     if (n==0)return 0;
+    //     if (n==1) return 1;
+    //     else return fib(n-1)+fib(n-2);
+    // }
+
+    // Dynamic Programming : Bottom up spproach
     int fib(int n) {
-        if (n==0)return 0;
-        if (n==1) return 1;
-        else return fib(n-1)+fib(n-2);
+        if ((n<=1))return n;
+        vector<int> response (n+1,0);
+        response[1]=1;
+        for (int i =2; i<=n; i++){
+            response[i] = response[i-1]+response[i-2];
+        }   
+        return response[n];
     }
 };
