2600dd9a481fed4f5518a383a69d665ed66b72ca
ihritwik <hritwiks.tgcs@gmail.com>
2023-01-06 17:28:03 -0500

    array manipulation

---
+++ leetcode/1929. Concatenation of Array.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans(2*nums.size());
+        for(int i=0; i<nums.size(); i++){
+            ans[i] = nums[i];
+            ans[i+nums.size()] = nums[i];
+        }
+        return ans;
+    }
+};
