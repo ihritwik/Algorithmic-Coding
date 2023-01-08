1e7b343be95839478a2bf5899fab9984a3106007
ihritwik <hritwiks.tgcs@gmail.com>
2023-01-07 22:51:31 -0500

    dynamic sliding window

---
+++ leetcode/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> chars;
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        while (right < s.length()) {
+            char r = s[right];
+            chars[r]++;
+            while (chars[r] > 1) {
+                char l = s[left];
+                chars[l]--;
+                left++;
+            }
+            res = max(res, right - left + 1);
+            right++;
+        }
+        return res;
+    }
+};
