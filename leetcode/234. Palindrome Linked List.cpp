940c9cc6f15100db7ab1506a35f0714c7eadf11b
ihritwik <hritwiks.tgcs@gmail.com>
2023-01-07 19:43:45 -0500

    linked list palindrome

--- leetcode/234. Palindrome Linked List.cpp
+++ leetcode/234. Palindrome Linked List.cpp
@@ -3,13 +3,13 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-// struct ListNode {
-//     int val;
-//     ListNode* next;
-//     ListNode() : val(0), next(nullptr) {}
-//     ListNode(int x) : val(x), next(nullptr) {}
-//     ListNode(int x, ListNode *next) : val(x), next(next) {}
-// };
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution {
 public:
     ListNode* end_of_first_half (ListNode* curr){
