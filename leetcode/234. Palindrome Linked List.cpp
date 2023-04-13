f5020e9a6aa91a38941ccee5d4ce4d25820feb13
Hritwik Shukla <90064474+ihritwik@users.noreply.github.com>
2023-04-12 21:07:15 -0400

    Update 234. Palindrome Linked List.cpp

--- leetcode/234. Palindrome Linked List.cpp
+++ leetcode/234. Palindrome Linked List.cpp
@@ -12,49 +12,44 @@ struct ListNode {
 };
 class Solution {
 public:
-    ListNode* end_of_first_half (ListNode* curr){
-        ListNode* fast_pointer = curr;
-        ListNode* slow_pointer = curr;
-            while((fast_pointer->next!=NULL)&& (fast_pointer->next->next!=NULL)){
-            fast_pointer = fast_pointer->next->next;
-            slow_pointer = slow_pointer->next;
+    ListNode* find_second_half_start_node(ListNode* temp){
+        ListNode* fast_pointer= temp;
+        ListNode* slow_pointer= temp;
+        while(fast_pointer->next!=NULL && fast_pointer->next->next!=NULL){
+            fast_pointer=fast_pointer->next->next;
+            slow_pointer=slow_pointer->next;
         }
         return slow_pointer->next;
     }
 
-    ListNode* reverseList (ListNode* head){
-        ListNode* prev_node = NULL;
-        ListNode* curr_node = head;
-        while(curr_node != NULL){    
-            ListNode* temp_node = curr_node->next;
-            curr_node->next = prev_node;
-            prev_node = curr_node;
-            curr_node = temp_node;      
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* flag = head;
+        while(curr!=NULL){
+            flag = curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=flag;
         }
-        //cout<<"Reverse list starting node = "<<prev_node->val<<endl;
-        return prev_node;
+        return prev;
     }
 
     bool isPalindrome(ListNode* head) {
-        if (head->next==NULL) return true;
-        ListNode* currNode = head;
-        ListNode* first_half_end_node =  end_of_first_half(currNode);
-        ListNode* second_half_start_node = reverseList(first_half_end_node);
+        bool response=true;
+        ListNode* second_half_start = find_second_half_start_node(head);
+        ListNode* second_half_reversed_head = reverseList(second_half_start);
+
         ListNode* ptr1 = head;
-        ListNode* ptr2 = second_half_start_node;
-        //cout<<"ptr1_val = "<<ptr1->val<<endl;
-        //cout<<"ptr2_val = "<<ptr2->val<<endl;
-        bool result = true;
-        while ((ptr2 != NULL)&&(result!=false)){
+        ListNode* ptr2 = second_half_reversed_head;
+
+        while ((ptr2!=NULL)&&(response!=false)){
             if (ptr1->val == ptr2->val){
-                ptr1 = ptr1->next;//Definition for singly-linked list.
-
-
-                ptr2 = ptr2->next; 
+                ptr1=ptr1->next;
+                ptr2=ptr2->next;
             }
-            else
-                result = false;
+            else response = false;
         }
-        return result;
+        return response;
     }
 };
